14bb9e479e74731a76cb803ee7f3becc1407610d
fitiarivony <fitiarivony2002@gmail.com>
2023-05-04 21:24:31 +0300

    Update updatearticle.blade.php

--- resources/views/updatearticle.blade.php
+++ resources/views/updatearticle.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="{{ url('/stats/ckeditor/ckeditor.js') }}" ></script>
-    <link href="{{url('/stats/ckeditor/contents.css') }}" rel="stylesheet">
+    <script src="{{ secure_url('/stats/ckeditor/ckeditor.js') }}" ></script>
+    <link href="{{ secure_url('/stats/ckeditor/contents.css') }}" rel="stylesheet">
 </head>
 <body>
     <form action="/updatearticle" method="post">
@@ -15,7 +15,7 @@
     <input type="hidden" value="{{ $article->id }}" name="id" />
     Resume:  <input type="text" name="resume" value="{{ $article->resumee }}"/>
     Categorie:<select name="categorie">
-        
+
         @foreach ($categories as $categorie)
        <option value="{{ $categorie->id }}">{{ $categorie->nomcategorie }}</option>
     @endforeach
